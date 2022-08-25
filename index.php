ba3e11b72263bb947b9872de439a39475a1b3757
Dave2.0 <olaniyibada462@gmail.com>
2022-08-25 14:45:16 +0100

    meta tags

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Freight Forwarding in Lagos, Freight Forwarding in Nigeria, send goods">
+    <meta name="description" content="Sunny Wealth Limited is an international Freight Forwarding and Shipping Agency">
     <title>Sunny Wealth Limited</title>
     <!-- Style Sheet -->
     <link rel="stylesheet" href="style.css">
